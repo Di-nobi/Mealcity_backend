6e51d17ca150d4e73344fbb56d613d8382050efa
Dinobi <architectstan01@gmail.com>
2023-12-24 20:41:12 +0100

    Database setup completed

---
+++ backend/1-main.js
@@ -0,0 +1,30 @@
+import dbClient from './utils/database';
+
+const waitConnection = () => {
+    return new Promise((resolve, reject) => {
+        let i = 0;
+        const repeatFct = async () => {
+            await setTimeout(() => {
+                i += 1;
+                if (i >= 10) {
+                    reject()
+                }
+                else if(!dbClient.isAlive()) {
+                    repeatFct()
+                }
+                else {
+                    resolve()
+                }
+            }, 1000);
+        };
+        repeatFct();
+    })
+};
+
+(async () => {
+    console.log(dbClient.isAlive());
+    await waitConnection();
+    console.log(dbClient.isAlive());
+    console.log(await dbClient.getUsers());
+    console.log(await dbClient.getWaitlist());
+})();
